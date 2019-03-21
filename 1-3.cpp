0e131dd2799b15ccba1660f5ce89584a315d9644
Atukholko <43937413+Atukholko@users.noreply.github.com>
2019-03-21 16:24:19 +0300

    Update 1-3.cpp

--- 1-3.cpp
+++ 1-3.cpp
@@ -1,10 +1,10 @@
-﻿//1.3.Элементы массива А(N), значения которых – простые числа, расположить в порядке возрастания, 
+//1.3.Элементы массива А(N), значения которых – простые числа, расположить в порядке возрастания, 
 //не нарушая порядка следования других элементов.
 #include<iostream>
 #include<time.h>
 using namespace std;
-int * pA = nullptr;
 int* giveMemory(int N) {
+	int * pA = nullptr;
 	pA = new(nothrow) int[N];
 	if (!pA) {
 		cout << "error";
@@ -52,35 +52,44 @@ bool isPrimeNumber(int number)
 	}
 	return isPrime;
 }
-void transpositionLeft(int *arr,  int N, int i, int k)
+void transpositionLeft(int *arr, int N, int i, int k)
 {
 
-	for (int j = N -1- k; j <N ;++j)
+	for (int j = N - 1 - k; j <N;++j)
 	{
-		*(arr+j) = *(arr + j+1);
+		*(arr + j) = *(arr + j + 1);
 	}
 }
-int main() {
-	int N{ 0 }, cnt{ 0 },tmp{0},k{0};
-	cout << "Enter array dimension\n";
-	cin >> N;
-	giveMemory(N);
-	randomInit(pA,N);
-	printArr(pA, N);cout << endl;
-	for (int i = N-1; i >= 0; --i) {			//Переносит простые числа в право
+int PrimeToTheEnd(int *pA, int N) {
+	int tmp = 0, k = 0, cnt = 0;
+	for (int i = N - 1; i >= 0; --i) {			//Переносит простые числа в право
 		if (isPrimeNumber(*(pA + i))) {
 			tmp = *(pA + i);
-			transpositionLeft(pA,N,i,k);
+			transpositionLeft(pA, N, i, k);
 			*(pA + N - 1) = tmp;
 			cnt++;
 		}
 		k++;
 	}
+	return cnt;
+}
+void sortPrime(int *pA, int cnt, int N) {
+	for (int i = 0; i < cnt; ++i)			//Расставляет простые в порядке возрастания
+		for (int j = N - cnt; j < N - 1; ++j)
+			if (*(pA + j)>*(pA + j + 1))
+				swap(*(pA + j), *(pA + j + 1));
+}
+int main() {
+	int N{ 0 }, cnt{ 0 };
+	int * pA = nullptr;
+	cout << "Enter array dimension\n";
+	cin >> N;
+	pA = giveMemory(N);
+	randomInit(pA, N);
 	printArr(pA, N);cout << endl;
-	for (int i = 0 ; i < cnt; ++i)			//Расставляет простые в порядке возрастания
-	for (int j = N-cnt; j < N-1; ++j)
-		if(*(pA + j)>*(pA + j +1))
-			swap(*(pA + j), *(pA + j + 1));
+	cnt = PrimeToTheEnd(pA, N);
+	PrimeToTheEnd(pA, N);
+	sortPrime(pA, cnt, N);
 	printArr(pA, N);cout << endl;
 	freeMemory(pA);
 	system("pause");
