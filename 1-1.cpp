c47fe5b1eb592b67bbac5ff8c3be8dde094c8910
Atukholko <43937413+Atukholko@users.noreply.github.com>
2019-03-27 19:18:16 +0300

    Update 1-1.cpp

--- 1-1.cpp
+++ 1-1.cpp
@@ -19,8 +19,8 @@ void distance(int *d, int * X, int *Y, int A, int B, int C, int N) {
 	}
 }
 void sortCoords(int * d, int * X, int * Y, int N) {
-	int flag = 0;
 	for (int j = 0; j < N - 1; ++j){
+		int flag = 0;
 		for (int i = 0; i < N - j - 1; ++i) {
 			if (d[i] > d[i + 1])
 			{
